79ce7f62a785e91c2ead37bb53d3217e287e1dc5
dme998 <dme998@mail.umsl.edu>
2020-09-17 06:05:46 -0500

    read in command line option and either print value or basic usage error

--- proc_fan.cpp
+++ proc_fan.cpp
@@ -9,11 +9,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <getopt.h>
 #include <sys/wait.h>
 
+using namespace std;
+
+void printUsage() {
+  cout << "Usage: proc_fan -n <number of processes>" << endl;
+  exit(1);
+}
+
+//arc is argument count, argv is array that contains the specific args
 int main(int argc, char *argv[]) {
-  std::cout << "hello world" << std::endl; 
+  /*
+  // parse command line options
+  if(argc==1)
+    cout << "Usage: ./program_name arg1" << endl;
+  else {
+    for(int i=1;i<argc; i++) {
+      cout << argv[i] << endl;
+    }
+  }
+  */
   
+  if (argc < 3) {
+    printUsage();
+  }
+
+  int option;
+  while ( (option = getopt(argc, argv, "n:") ) != -1) {
+    switch(option) {
+      case 'n':
+        cout << argv[2] << endl;
+        break;
+      default:
+        printUsage();
+        break;
+    }
+  }
+
   return 0;
 
 }
