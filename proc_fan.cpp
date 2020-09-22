f39d830c2d445c45a3800aa0ed2d87a2304afc30
dme998 <dme998@mail.umsl.edu>
2020-09-22 16:29:41 -0500

    Now properly execs file as parameter: ./proc_fan -n 5 ./testsim

--- proc_fan.cpp
+++ proc_fan.cpp
@@ -20,58 +20,125 @@ void printUsage() {
   exit(1);
 }
 
+void printError() {
+  cout << "Generic error message." << endl;
+}
+
 //arc is argument count, argv is array that contains the specific args
 int main(int argc, char *argv[]) {
-  /*
-  // parse command line options
-  if(argc==1)
-    cout << "Usage: ./program_name arg1" << endl;
-  else {
-    for(int i=1;i<argc; i++) {
-      cout << argv[i] << endl;
-    }
-  }
-  */
-  
-  if (argc < 3) {
-    printUsage();
-  }
 
   int option;       // user command line option
-  int pr_count = 0; // number of active children
-  int pr_limit = 3; // max number of children allowed to execute at a time (when one ends, launch another)
-  while ( (option = getopt(argc, argv, "N:n:") ) != -1 ) {
+  int pr_count = 0; // number of current active children
+  int pr_limit = 3; // max  children allowed to execute at a time (when one ends, launch another)
+  
+  while ( (option = getopt(argc, argv, "n:") ) != -1 ) {
+    
+    if (optind >= argc) {
+      printUsage();
+      exit(1);
+    }
+
     switch(option) {
-      case 'N':
       case 'n': {
-        int n = atoi(argv[2]);  
-        for(int i = 0; i < n; i++) {
+          int n = atoi(argv[(optind-1)]); //for parsing char array to int
+          //for-loop encasing switch? doesnt getopt already loop through all args?
+          /*
+          cout << "argv[optind-1] as n: " <<  n << endl;
+          cout << "argv[optind-1]     : " << (optind-1) << endl;
+          cout << "argv[optind]       : " << argv[optind] << endl;
+          cout << "optind             : " << (optind) << endl;
+          
+          // ARGV[OPTIND] IS THE FILENAME (PROCESS) THAT YOU CAN EXEC
+          */
+          
+          /* Fork it. Should return >= 0 if successful, -1 if error */
           pid_t pid = fork();
-          if(pid == -1) // child should return >= 0 if successful fork, -1 if error
-            {perror("fork");}
+          if(pid == -1) {
+            perror("perror: fork failed."); 
+          }
           else if(pid == 0) {
-            cout << "I am a Child, PID: " << pid << endl;
-            sleep(1);
+            //child
+            cout << "I am a child, pid: " << pid << endl;
+            
+            //exec <argv[optind]>
+            sleep(1); 
+            char *args[2];
+            string program = argv[optind]; //get filename param that we will exec
+            args[0] = (char*)program.c_str(); //convert type for use in exec
+            args[1] = NULL; //array must end in NULL to avoid seg fault
+            if( execvp(args[0],args) == -1 ) {
+              perror("perror: exec.");
+            }
+            
+            cout << "Child now ending (if you see this message, exec didnt work)." << endl;
             exit(0);
           }
           else if(pid > 0) {
             //parent
-            cout << "I am Parent, PID: "<< pid << endl;
-            wait(NULL); 
+            cout << "I am a parent, pid: " << pid << endl;
+            wait(NULL);
+            cout << "Parent done waiting" << endl;
           }
-            
-        } 
+          
+        }
         break;
-      }
-      
-      
-      
+      case '?': 
+        perror("perror: case ?.");
+        break;
       default:
-        printUsage();
+        perror("perror: case default.");
         break;
+
+
     }
+    
   }
 
+
+
+
+
+
+
+
+
+/*  while ( (option = getopt(argc, argv, "N:n:") ) != -1 ) {
+    for(int i = 3; i <= argc; i++) {
+      switch(option) {
+        case 'N':
+        case 'n': {
+          int n = atoi(argv[(i-1)]);  //for parsing char array to int
+          //cout << "Fanning given process, " << argv[i] << ", " << n << " times :" << endl;
+        for(int j = 0; j < n; j++) {
+            pid_t pid = fork();
+            if(pid == -1) // child should return >= 0 if successful fork, -1 if error
+              {perror("error: fork failed.");}
+            else if(pid == 0) {
+              //cout << "I am a Child, PID: " << pid << endl;
+              
+              char *args[] = {"./fanwaitmsg","2", NULL};
+              execv("./fanwaitmsg",args);
+              
+              exit(0);
+            }
+            else if(pid > 0) {
+              //parent
+              //cout << "I am Parent, PID: "<< pid << endl;
+              wait(NULL);
+              //cout << "I am done waiting." << endl;
+            }
+              
+          }
+          break;
+        }     
+        default:
+          printUsage();
+          break;
+      }
+      //cout << endl;
+    }
+  }
+*/
   return 0;
 
 }
